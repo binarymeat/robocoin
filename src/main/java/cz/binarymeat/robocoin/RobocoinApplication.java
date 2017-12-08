d242e1d72f846ee1d7d2aaa91032882335336a12
Robert Jakes <robert.jakes@iongroup.com>
2017-12-08 14:40:10 +0100

    init

---
+++ src/main/java/cz/binarymeat/robocoin/RobocoinApplication.java
@@ -0,0 +1,12 @@
+package cz.binarymeat.robocoin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobocoinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RobocoinApplication.class, args);
+	}
+}
